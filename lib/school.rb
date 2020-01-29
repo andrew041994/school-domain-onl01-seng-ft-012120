21716e55d372b2f031866e735fc5d9b0457af2a0
Alehandro Persaud <andrew041994@gmail.com>
2020-01-29 15:47:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor
+  def initialize(school)
+    @school = school
+  end
+
+  def school=(school)
+    @school = school
+  end
+
+  def school
+    @school
+  end
+  def roster=(roster={})
+    # roster={}
+    @roster = roster
+
+  end
+
+  def roster
+    @roster
+  end
+
+end
